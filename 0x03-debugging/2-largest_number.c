0df72751bae17b0d5388d5dedf1e8de1c205d522
Desta Gebremariyam <desta1gmariyam@gmail.com>
2022-07-07 14:21:17 -0700

    0x03-debugging task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,19 +14,19 @@ int largest;
 
 if (a > b && a > c)
 {
-largest = a;
+        largest = a;
 }
-else if (b > a && c > a)
+else if (b > a && b > a)
 {
-largest = c;
+        largest = b;
 }
-else if (b > c)
+else if (c > b)
 {
-largest = b;
+        largest = c;
 }
 else
 {
-largest = c;
+        largest = b;
 }
 
 return (largest);
---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+* _isupper -> this is a function to check upper case alphas
+* @c: an integer paramer
+* Return: something but success
+*/
+int _isreturn (c >= 'A' && c <= 'Z');
+}

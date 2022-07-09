b31a1a920fb02964270546a3cbb7ead2191d4f36
Desta Gebremariyam <desta1gmariyam@gmail.com>
2022-07-08 18:25:12 -0700

    0x04-more_functions_nested_loops task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * _isupper - a function to check upper case 
 * @c: print int
@@ -6,15 +7,6 @@
 */
 
 int _isupper(int c)
-}
-if (c => 'A' && c <= 'Z')
 {
-return (1);
-}
-
-else
-{
-return (0);
-}
-
+return (c => 'A' && c <= 'Z');
 }
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -7,6 +7,6 @@
 * Return: on success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
-int putcharurn (write(1, &c, 1));
+int _putchar(char c)
 {
-	return (
+return (write(1, &c, 1));

9a434d51199459ff9ab2c2f1e3613ea6fae47cb9
Desta Gebremariyam <desta1gmariyam@gmail.com>
2022-07-11 05:21:10 -0700

    0. 98 Battery st task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* 0-rest_to_98.c - update the value it points to 98
+* rest_to_98 -> a function to update the value it points to 98
 * @n: pointer n to be updated
 */
 void reset_to_98(int *n)
